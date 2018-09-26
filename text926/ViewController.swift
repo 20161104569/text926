1c66225e5a0ab6b466a9b511db833962fd339dc1
20161104569 <2174037646@qq.com>
2018-09-26 11:29:31 +0800

    输出hello world
    输出hello world

--- text926/ViewController.swift
+++ text926/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var gxtx: UITextField!
+    @IBAction func test(_ sender: Any) {
+         gxtx.text = "hello world"
+    }
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
